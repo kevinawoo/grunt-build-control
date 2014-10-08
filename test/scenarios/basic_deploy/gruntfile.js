b231ea65b3b43db49421252fffd778ea1747a19f
Kevin Woo <kevinawoo@gmail.com>
2014-10-08 18:12:54 -0500

    execute tests with scenarios

---
+++ test/scenarios/basic_deploy/gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+	// add custom tasks
+	grunt.loadTasks('../../../tasks');
+
+
+	// test config
+	grunt.initConfig({
+		buildcontrol: {
+			options: {
+				dir: 'dist',
+				remote: '../remote'
+			},
+			testing: {
+				options: {
+					branch: 'master',
+					commit: true,
+					push: true,
+					connectCommits: false
+				}
+			}
+		}
+	});
+
+	// default task
+	grunt.registerTask('default', ['buildcontrol']);
+};
