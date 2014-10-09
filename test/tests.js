948d4ba4a92d9168c24cd021e3d868e58bf09144
Kevin Woo <kevinawoo@gmail.com>
2014-10-08 19:12:00 -0500

    added comments

--- test/tests.js
+++ test/tests.js
@@ -11,6 +11,17 @@ var gruntExec = 'node ' + path.resolve('node_modules/grunt-cli/bin/grunt');
 var should = require('chai').should();
 
 
+/**
+ * Executes a Scenario given by tests.
+ *
+ * - A scenario starts with a `gruntfile.js` configuration.
+ * - Each build task will upload to a mock repo (folder name is `remote`)
+ * - It will then clone the remote.
+ * - Validations are done on the `verify` folder
+ *
+ * NOTE: this function DOES change the process's working directory to the `scenario` so that
+ * validations are easier access.
+ */
 var execScenario = function (scenario, cb) {
 	process.chdir(__dirname + '/scenarios/' + scenario);
 
@@ -62,6 +73,11 @@ var execScenario = function (scenario, cb) {
 
 
 
+/**
+ * Tests
+ *
+ * Assume that each test (`it`) is in the current working directory of the scenario.
+ */
 describe('build control tests', function () {
 
 	it('should do a basic deployment', function (done) {
